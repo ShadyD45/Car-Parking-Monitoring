006c8b24c97f4d0252a381cb95f773bb924d88f8
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2020-06-29 12:03:53 +0530

    Update read_port.c

--- read_port.c
+++ read_port.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/* This function is used to read the sensor data from the Serial port (USB Port) */
 void read_port(int st[])
 {
 	int i=0;
 	FILE *fp;
-	fp=fopen("serial.txt","r");	//fp=fopen("/dev/ttyACM0","r");
+	/*
+	  Uncomment only for testing purposes for this funcion
+		fopen("serial.txt","r");
+	*/
+	fp=fopen("/dev/ttyACM0","r");	//The file ttyACM0,ttyACM1...ttyACMx stores the data received from the sensors
 	while((st[i]=fgetc(fp)) != '\n')
 	{
 		if(st[i]==48 || st[i]==49)
